c13de18e5d7342cf379e7e2505cbe0f6ef4ba0d4
roger <roger@localhost.localdomain>
2016-11-27 15:25:45 -0600

    update active model serializers configuration

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Pruebarails2
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    ActiveModelSerializers.config.adapter = :json
   end
 end
