3aafa06c87d5a2c8ce89bc57b64c1c031b08a733
roger <roger@localhost.localdomain>
2016-11-28 11:49:37 -0600

    add order create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,16 @@ class OrdersController < ApplicationController
   def show
     render json: Order.find(params[:id]), serializer: OrderDetailSerializer
   end
+  
+  def create
+    customer = Customer.find params[:customer_id]
+    products = Product.find params[:product_ids]
+    order = Order.create(customer: customer)
+    order.products << products
+    if order.save
+      render 201
+    else
+      render 400
+    end  
+  end
 end
