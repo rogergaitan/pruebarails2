720752d475229ddaf12a955eec1e60fbe0c4a4b0
roger <roger@localhost.localdomain>
2016-11-28 11:49:52 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index, :show]
   resources :products, only: [:index, :show]
+  resources :orders, only: [:show, :create]
 end
