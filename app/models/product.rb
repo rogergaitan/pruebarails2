1690d4ec7e9d022791b30d9ce877645f6d352ee3
roger <roger@localhost.localdomain>
2016-11-28 12:39:22 -0600

    update models validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :product_category
   has_and_belongs_to_many :orders
+  
+  validates :name, :price, presence: true
+  validates :price, numericality: { greater_than: 0.0 }
 end
