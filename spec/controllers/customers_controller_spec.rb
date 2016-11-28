f60f259f91b72bd6e051491884ad541d081a803e
roger <roger@localhost.localdomain>
2016-11-28 10:29:54 -0600

    add customer controller show action specs

---
+++ spec/controllers/customers_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe CustomersController, type: :controller do
+  describe "GET #index" do
+    it "returns response status 200" do
+      get :index
+      expect(response).to be_success
+    end
+    
+    it "returns list of customers" do
+      FactoryGirl.create_list :customer, 10
+      get :index
+      json = JSON.parse response.body
+      expect(json['customers'].length).to eq(10)
+    end
+  end
+  
+  describe "GET #show" do
+    before do
+      @customer = FactoryGirl.create :customer
+    end
+      
+    it "returns response status 200" do
+      get :show, id: @customer  
+      expect(response).to be_success
+    end
+    
+    it "return correct content" do
+      product_category = FactoryGirl.create :product_category
+      product = FactoryGirl.create :product, product_category: product_category 
+
+      order = FactoryGirl.create(:order, customer: @customer)
+      order.products << product
+      
+      @customer.orders << order
+      
+      get :show, id: @customer
+      json = JSON.parse response.body
+      expected_response = {"customer"=>{"id"=>1, "name"=>"test", "email"=>"test@12test.com", "address"=>"test address", "phone"=>"55566444", "orders"=>[{"id"=>1}]}}
+
+      expect(json).to eq(expected_response)
+    end
+  end
+
+end
