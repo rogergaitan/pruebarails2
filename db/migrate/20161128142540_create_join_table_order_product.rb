207925007eb1f1717ddab8d44eeda966762b1907
roger <roger@localhost.localdomain>
2016-11-28 10:30:27 -0600

    add product order join table migration

---
+++ db/migrate/20161128142540_create_join_table_order_product.rb
@@ -0,0 +1,7 @@
+class CreateJoinTableOrderProduct < ActiveRecord::Migration[5.0]
+  def change
+    create_join_table :orders, :products do |t|
+      t.index [:order_id, :product_id]
+    end
+  end
+end
