8dbe1567be41297d16d7a7986cecfd5a8e02f6c1
roger <roger@localhost.localdomain>
2016-11-28 12:46:17 -0600

    update customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders
   
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, :email, :address, :phone, presence: true
   validates :email, uniqueness: true
-  validates :address, presence: true
-  validates :phone, presence: true
   validates :phone, numericality: { only_integer: true }
 end
